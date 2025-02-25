8b8312384b79dec5dbe6992c80c6f2f5f53c672b
davher.ext <davher.ext@letiempo.com>
2025-02-25 16:13:53 -0500

    Piedra papel Tijera

---
+++ src/main/java/org/example/Juego.java
@@ -0,0 +1,69 @@
+package org.example;
+
+import java.util.Random;
+
+public class Juego {
+    public static void main(String[] args) {
+        int contadorJuegos = 0;
+        int contadorJugador1 = 0;
+        int contadorJugador2 = 0;
+        int rondas=3;
+        Random random = new Random();
+
+        do {
+            int jugador1 = random.nextInt(3) + 1;
+            int jugador2 = random.nextInt(3) + 1;
+            String denoJugador1 = "";
+            String denoJugador2 = "";
+            if (jugador1 == 1) {
+                denoJugador1 = "Piedra";
+            } else if (jugador1 == 2) {
+                denoJugador1 = "Papel";
+            } else if (jugador1 == 3) {
+                denoJugador1 = "Tijera";
+            }
+
+            if (jugador2 == 1) {
+                denoJugador2 = "Piedra";
+            } else if (jugador2 == 2) {
+                denoJugador2 = "Papel";
+            } else if (jugador2 == 3) {
+                denoJugador2 = "Tijera";
+            }
+
+            if (jugador1 == jugador2) {
+                System.out.println("Jugador 1: " + denoJugador1+" - Jugador 2: "+denoJugador2);
+                System.out.println("Empate");
+                rondas++;
+            } else if (jugador1 == 1 && jugador2 == 2) {
+                System.out.println("Jugador 1: " + denoJugador1+" - Jugador 2: "+denoJugador2);
+                System.out.println("Gana Jugador 2");
+                contadorJugador2++;
+            } else if (jugador1 == 1 && jugador2 == 3) {
+                System.out.println("Jugador 1: " + denoJugador1+" - Jugador 2: "+denoJugador2);
+                System.out.println("Gana Jugador 1");
+                contadorJugador1++;
+            } else if (jugador1 == 2 && jugador2==1) {
+                System.out.println("Jugador 1: " + denoJugador1+" - Jugador 2: "+denoJugador2);
+                System.out.println("Gana Jugador 1");
+                contadorJugador1++;
+            } else if (jugador1==2  && jugador2==3) {
+                System.out.println("Jugador 1: " + denoJugador1+" - Jugador 2: "+denoJugador2);
+                System.out.println("Gana Jugador 2");
+                contadorJugador2++;
+            } else if (jugador1 == 3 && jugador2 == 1) {
+                System.out.println("Jugador 1: " + denoJugador1+" - Jugador 2: "+denoJugador2);
+                System.out.println("Gana Jugador 2");
+                contadorJugador2++;
+            } else if (jugador1 == 3 && jugador2 == 2) {
+                System.out.println("Jugador 1: " + denoJugador1+" - Jugador 2: "+denoJugador2);
+                System.out.println("Gana Jugador 1");
+                contadorJugador1++;
+            }
+            contadorJuegos++;
+            if (contadorJugador1==2 || contadorJugador2==2){
+                break;
+            }
+        } while (contadorJuegos <= rondas);
+    }
+}
